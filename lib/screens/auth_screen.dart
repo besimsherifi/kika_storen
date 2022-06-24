e90e63ecddb81878d88195c696694695f6ac548c
besimsherifi <sherifibesim@gmail.com>
2022-06-24 23:47:11 +0200

    commit: UI(Auth-Screen) *created the authentication page

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,1 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 100),
+                        child:
+                            Image(image: AssetImage('assets/images/logo.png')),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Willkomen',
+                        style: GoogleFonts.anekLatin(
+                          textStyle: const TextStyle(fontSize: 22),
+                          color: const Color(0xff164276),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              border: Border.all(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Padding(
+                            padding: EdgeInsets.only(left: 20.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  fillColor: Colors.white,
+                                  hintText: 'username'),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              border: Border.all(color: Colors.white),
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Padding(
+                            padding: EdgeInsets.only(left: 20.0),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  fillColor: Colors.white,
+                                  hintText: 'password'),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Container(
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                              color: const Color(0xff0991B5),
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Center(
+                              child: Text(
+                            'Anmelden',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
