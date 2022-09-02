1bf71aa6f6e84d39f5d1e38e2c2a88ba33c2c6d4
besimsherifi <sherifibesim@gmail.com>
2022-09-02 16:30:01 +0200

    commit:Screens(Contacts) #25 *bug fixed

--- lib/screens/Contacts/contacts_list_screen.dart
+++ lib/screens/Contacts/contacts_list_screen.dart
@@ -1,54 +1,33 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ContactsListScreen extends StatefulWidget {
-  const ContactsListScreen(this.contact);
+class ContactsListScreen extends StatelessWidget {
+  const ContactsListScreen(this.contact, {Key? key}) : super(key: key);
 
   final String contact;
 
   @override
-  State<ContactsListScreen> createState() => _ContactsListScreenState();
-}
-
-class _ContactsListScreenState extends State<ContactsListScreen> {
-  final contacts = FirebaseFirestore.instance
-      .collection('test')
-      .doc('zVDTxxrQTWPBh90B6EYa')
-      .collection('Intern')
-      .snapshots();
-  dynamic data;
-
-  @override
-  void initState() {
-    super.initState();
-    readUsers();
-  }
-
-  readUsers() async {
-    var db = FirebaseFirestore.instance.collection('test');
-    data = db.doc('Extern').snapshots();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final contacts = FirebaseFirestore.instance
+        .collection('contacts')
+        .doc('G7zz3UnSiNJpqUpWJyF1')
+        .collection(contact)
+        .snapshots();
     return StreamBuilder<QuerySnapshot>(
       stream: contacts,
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasError) {
-          return Text('Something went wrong');
+          return const Text('Something went wrong');
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
+          return const Text("Loading");
         }
 
         return ListView(
           children: snapshot.data!.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data =
                 document.data()! as Map<String, dynamic>;
-            print(data);
             return ListTile(
               title: Text(data['name']),
             );
